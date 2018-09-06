ce989b39acccb4b9d062c42d296ee21e8b675180
gaoq <gaoq@rose-hulman.edu>
2018-09-05 20:02:29 -0400

    33

--- src/m7_summing.py
+++ src/m7_summing.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and BERT.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +28,19 @@ def run_test_sum_cosines():
     print('--------------------------------------------------')
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
+    expected = 1.1241
+    answer = sum_cosines(2)
+    print('The expected is', expected)
+    print('The real answer is', answer)
+    expected = 0.13416
+    answer = sum_cosines(3)
+    print('The expected is', expected)
+    print('The real answer is', answer)
+    expected = -0.2358
+    answer = sum_cosines(5)
+    print('The expected is', expected)
+    print('The real answer is', answer)
+
 
 
 def sum_cosines(n):
@@ -41,19 +54,23 @@ def sum_cosines(n):
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_cosines  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
-
+    import math as mt
+    sum = 1
+    for k in range (n):
+        sum = sum +mt.cos(k+1)
+    return sum
 
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -64,6 +81,18 @@ def run_test_sum_square_roots():
     print('--------------------------------------------------')
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
+    expected = 3.4142
+    answer = sum_square_roots(2)
+    print('The expected is', expected)
+    print('The real answer is', answer)
+    expected = 5.8637
+    answer = sum_square_roots(3)
+    print('The expected is', expected)
+    print('The real answer is', answer)
+    expected = 11.8544
+    answer = sum_square_roots(5)
+    print('The expected is', expected)
+    print('The real answer is', answer)
 
 
 def sum_square_roots(n):
@@ -80,14 +109,17 @@ def sum_square_roots(n):
       which is about 11.854408.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_square_roots  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
-
+    sum = 0
+    for k in range(n):
+        sum = sum + (2*(k+1))**(1/2)
+    return sum
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
