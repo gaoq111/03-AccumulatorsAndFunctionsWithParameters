47f8779aae07a5a46e461b3ed5c1121015875a12
gaoq <gaoq@rose-hulman.edu>
2018-10-01 08:38:36 -0400

    33

--- src/m2r_using_rosegraphics_objects.py
+++ src/m2r_using_rosegraphics_objects.py
@@ -5,7 +5,7 @@ This module uses ROSEGRAPHICS to demonstrate:
   -- accessing their DATA via INSTANCE VARIABLES.
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and BERT.
+         their colleagues and BERt.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
